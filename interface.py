f5272c7967f48edd06aa6e47a9a285e067a58210
Nghia Tran Dai <nghiatd.proptit@gmail.com>
2018-09-13 15:24:30 +0700

    interface for training online

--- interface.py
+++ interface.py
@@ -1,28 +1,42 @@
 from tkinter import *
 import _thread
 import time
-
+import vision_config
+import logging
+from interact_database_v2 import Database
+from object_DL import Person
 STATUS_INACTIVE = 0
 STATUS_INPROGRESS = 1
 STATUS_DONE = 2
 STATUS = STATUS_INACTIVE
-label = None
+STATUS_CONFIRM = 3
+result = None
 
-def response_OK(text):
-    global STATUS, label
-    label = text
-    print(label)
+db = Database(vision_config.DB_HOST, vision_config.DB_USER, vision_config.DB_PASSWD, vision_config.DB_NAME)
+def response_idCode_OK(idCode):
+    person = db.getPersonByIdCode(idCode)
+    if person is None:
+        get_information(idCode)
+    else:
+        show_information("Transfer Learning", person)
+    # STATUS = STATUS_DONE
+
+def response_learning_OK(msg, person):
+    global STATUS
+    logging.info("{} | person [id: {} - name: {} - idCode: {} - age: {} - gender: {}]".format(msg, \
+                                                                    person.id, person.name, person.idcode, person.age, person.gender))
+    result = person
     STATUS = STATUS_DONE
 
 def response_cancel():
     global STATUS
     STATUS = STATUS_INACTIVE
-    label = None
+    result = None
 
 def reset():
     global STATUS
     STATUS = STATUS_INACTIVE
-    label = None
+    result = None
 
 def center(win):
     """
@@ -41,33 +55,108 @@ def center(win):
     win.geometry('{}x{}+{}+{}'.format(width, height, x, y))
     win.deiconify()
 
+def show_information(msg, person):
+    name = person.name
+    idCode = person.idcode
+    age = person.age
+    gender = person.gender
 
-def layout_text():
+    global STATUS
+    STATUS = STATUS_CONFIRM
+    label = None
+    root = Tk()
+    root.title("Information")
+    # root.geometry("500x50+100+100")
+    row = Frame(root)
+    width_box = int(len(name)) + 10
+    notice = Label(row, text=msg, width=width_box, font=(16), anchor='w')
+    notice.pack(side=TOP)
+
+    idCode_lb = Label(row, text="ID Code: {}".format(idCode), width=width_box, font=(16), anchor='w')
+    idCode_lb.pack(side=TOP)
+
+    name_lb = Label(row, text="Full Name: {}".format(name), width=width_box, font=(16), anchor="w")
+    name_lb.pack(side = TOP)
+    
+    age_lb = Label(row, text="Age: {}".format(age), width=width_box, font=(16), anchor="w")
+    age_lb.pack(side = TOP)
+
+    gender_lb = Label(row, text="Gender: {}".format(gender), width=width_box, font=(16), anchor="w")
+    gender_lb.pack(side = TOP)
+
+    row.pack(side=TOP, fill=X, padx=5, pady=5)
+    check = 0
+
+    def call_ok(event=None):
+        response_learning_OK(msg, person)
+        root.quit()
+        root.destroy()
+
+    def call_cancel(event=None):
+        global STATUS
+        STATUS = STATUS_INPROGRESS
+        root.quit()
+        root.destroy()
+    btn_ok = Button(root, text="Accept", width=10, command=call_ok)
+    btn_ok.pack(side=LEFT, padx=5, pady=5)
+    btn_cancel = Button(root, text="Cancel", width=10, command=call_cancel)
+    btn_cancel.pack(side=RIGHT, padx=5, pady=5)
+    root.bind('<Return>', call_ok)
+    root.bind('<Escape>', call_cancel)
+    center(root)
+    root.mainloop()
+    # root.destroy()
+def get_information(idCode):
     global STATUS
     STATUS = STATUS_INPROGRESS
     label = None
     root = Tk()
-    root.title("Thong Tin Nhan Vien")
+    root.title("New Person")
     # root.geometry("500x50+100+100")
     row = Frame(root)
-    lab = Label(row, text="Ma Nhan Vien", width=12, font=(16), anchor='w')
-    ent = Entry(row, width=18, font=16)
+    idCode_lb = Label(row, text="ID Code: {}".format(idCode), width=30, font=(16), anchor='w')
     # e1.grid(row=0, column = 1)
+    
+    idCode_lb.pack(side=TOP)
+
+    name_lb = Label(row, text="Full Name", width=12, font=(16), anchor="w")
+    name_lb.pack(side = TOP)
+
+    name_txt = Entry(row, width=18, font=16)
+    name_txt.pack(side=TOP, expand=YES, fill=X)
+    
+    age_lb = Label(row, text="Age", width=12, font=(16), anchor="w")
+    age_lb.pack(side = TOP)
+
+    age_txt = Entry(row, width=18, font=16)
+    age_txt.pack(side=TOP, expand=YES, fill=X)
+
+    gender_lb = Label(row, text="Gender", width=12, font=(16), anchor="w")
+    gender_lb.pack(side = TOP)
+
+    gender_txt = Entry(row, width=18, font=16)
+    gender_txt.pack(side=TOP, expand=YES, fill=X)
+
     row.pack(side=TOP, fill=X, padx=5, pady=5)
-    lab.pack(side=LEFT)
-    ent.pack(side=RIGHT, expand=YES, fill=X)
-    ent.focus_set()
     check = 0
 
     def call_ok(event=None):
-        if(len(ent.get()) == 0):
+        if STATUS == STATUS_CONFIRM:
             return
-        response_OK(ent.get())
-        root.quit()
+        if len(name_txt.get()) == 0 or len(age_txt.get()) == 0 or len(gender_txt.get()) == 0:
+            get_information(idCode)
+        new_person = Person(name=name_txt.get(), age= int(age_txt.get()), gender=gender_txt.get(), idcode=idCode)
+        # root.quit()
+        show_information("Learn New Person", new_person)
+        
+        if STATUS == STATUS_DONE:
+            root.destroy()
 
     def call_cancel(event=None):
+        if STATUS == STATUS_CONFIRM:
+            return
         response_cancel()
-        root.quit()
+        root.destroy()
     btn_ok = Button(root, text="OK", width=10, command=call_ok)
     btn_ok.pack(side=LEFT, padx=5, pady=5)
     btn_cancel = Button(root, text="Cancel", width=10, command=call_cancel)
@@ -76,7 +165,49 @@ def layout_text():
     root.bind('<Escape>', call_cancel)
     center(root)
     root.mainloop()
-    root.destroy()
-if __name__ = "__main__":
-    layout_text()
+    # root.destroy()
+
+
+def get_idCode():
+    global STATUS, label
+    STATUS = STATUS_INPROGRESS
+    label = None
+    root = Tk()
+    root.title("Log in")
+    # root.geometry("500x50+100+100")
+    row = Frame(root)
+    idCode_lb = Label(row, text="ID Code", width=12, font=(16), anchor='w')
+    idCode_txt = Entry(row, width=18, font=16)
+    # e1.grid(row=0, column = 1)
+    
+    idCode_lb.pack(side=TOP)
+    idCode_txt.pack(side=TOP, expand=YES, fill=X)
+    idCode_txt.focus_set()
+
+    row.pack(side=TOP, fill=X, padx=5, pady=5)
+    check = 0
+
+    def call_ok(event=None):
+        idCode = idCode_txt.get()
+        if not str.isdigit(idCode):
+            return
+        idCode = int(idCode)
+        root.destroy()
+        response_idCode_OK(idCode)
+        
+
+    def call_cancel(event=None):
+        response_cancel()
+        root.destroy()
+    btn_ok = Button(root, text="OK", width=10, command=call_ok)
+    btn_ok.pack(side=LEFT, padx=5, pady=5)
+    btn_cancel = Button(root, text="Cancel", width=10, command=call_cancel)
+    btn_cancel.pack(side=RIGHT, padx=5, pady=5)
+    root.bind('<Return>', call_ok)
+    root.bind('<Escape>', call_cancel)
+    center(root)
+    root.mainloop()
+    # root.destroy()
+if __name__ =="__main__":
+    get_idCode()
 # layout_text()
